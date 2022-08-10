7c226d27bcd03b7e24bc61e45df02e8a3cfac049
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-08-09 21:17:49 -0700

    added the mapping of table name and column names to correaponding entity and col

--- src/main/java/com/prateek/springbootwithjpa/entity/Student.java
+++ src/main/java/com/prateek/springbootwithjpa/entity/Student.java
@@ -5,16 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Builder
+@Table(name="tbl_student")
 public class Student {
     /**
      * Auto (default) - just a primary key and no auto generation
@@ -23,6 +21,7 @@ public class Student {
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(name="email_address")
     private String email;
     private String guardianName;
     private String guadianEmail;
