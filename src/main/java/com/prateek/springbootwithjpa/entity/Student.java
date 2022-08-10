12a78535fb0ec370e617217368ab0ffda407c068
Prateek Joshi <prateekjoshi2013@gmail.com>
2022-08-09 21:29:20 -0700

    added seaquence generation logic for id

--- src/main/java/com/prateek/springbootwithjpa/entity/Student.java
+++ src/main/java/com/prateek/springbootwithjpa/entity/Student.java
@@ -16,15 +16,27 @@ import javax.persistence.*;
 public class Student {
     /**
      * Auto (default) - just a primary key and no auto generation
+     *
+     * Sequence -  along with Sequence generator definition - creates a sequence with the table and
+     *  uses it for id gen
      */
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
     @Column(name="email_address")
     private String email;
     private String guardianName;
-    private String guadianEmail;
+    private String guardianEmail;
     private String guardianMobile;
 
 }
